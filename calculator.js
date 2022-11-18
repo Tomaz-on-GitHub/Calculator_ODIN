2b9a820a4c2dc297715938aab599a799beace5f1
Tomaz_on_git <gumimixer@gmail.com>
2022-11-18 15:00:17 +0100

    create functional calculator (+-*/, point, inversion, power, sign)

--- calculator.js
+++ calculator.js
@@ -1,5 +1,7 @@
 //create global value to store display value
-let displayValue = 0;
+let displayValue = undefined;
+let displayValueFirst = undefined;
+let OperationSign = '';
 
 
 const add = function(a,b) {
@@ -42,6 +44,10 @@ const inverse = function(num) {
   } else { return 1/num }
 };
 
+const sign = function(num) {
+  return -num;
+};
+
 const factorial = function(num) {
 	if (num === 0) { 
     return 1;
@@ -76,6 +82,8 @@ function operate(operator, a,b,) {
       break;
     case 'inverse':
       return inverse(a);
+    case 'sign':
+      return sign(a);
   }
 }
 
@@ -89,6 +97,9 @@ console.log(power(5,3));
 console.log(factorial(4));
 console.log(inverse(6));#
 */
+console.log('function subtract:' + operate('subtract',37,2));
+console.log('function sign:' + operate('sign',5));
+/*
 console.log('function add:' + operate('add',2,4));
 console.log('function subtract:' + operate('subtract',12,8));
 console.log('function multiply:' + operate('multiply',24,2));
@@ -96,6 +107,7 @@ console.log('function divide:' + operate('divide',10,2));
 console.log('function power:' + operate('power',5,3));
 console.log('function factorial:' + operate('factorial',4));
 console.log('function inverse:' + operate('inverse',6));
+*/
 //console.log(factorial(10));
 // Do not edit below this line
 /*
@@ -112,11 +124,97 @@ function writeNumber(num) {
   //console.log('you pressed: ' + element.id)
   //limit display to 10 numbers
   elDisplay = document.getElementById("display")
-  if (elDisplay.textContent.length < 10) {
-    if (displayValue == 0) {displayValue = num;
-    } else {displayValue += num;
+
+  if (OperationSign !=='' && displayValueFirst === undefined) {
+    //operation is selected, need to write the second number 
+    displayValueFirst = displayValue;
+    console.log('first value:' + displayValueFirst)
+    displayValue = undefined;  //reset the display
+ }
+  
+    if (displayValue == undefined) {
+      displayValue =num;
+    } else {
+      if (elDisplay.textContent.length < 11) {
+        displayValue += num;
+        }
+  }
+  elDisplay.textContent = displayValue;
+}
+
+function writeOperation(OpSign) {
+  //console.log('you pressed: ' + Opsign);
+  if (OperationSign ==='') { //sign pressed for the first time
+    OperationSign = OpSign;
+    console.log('you pressed: ' + OperationSign);
+    } else {//console.log('prepare for the operation');
+    //console.log('op:'+OperationSign + ' :' + displayValueFirst + ' :' + displayValue  );
+    executeOperation();
+    //new operation prepare
+    OperationSign = OpSign;
+    displayValueFirst = displayValue;
+    displayValue = undefined;
+    //console.log(''+OperationSign + ' ; ' + displayValueFirst + ' ; ' + displayValue  );
     }
   
-  elDisplay.textContent = displayValue;
+}
+
+function executeOperation() {
+  //the equl sign was pressed
+  if(displayValueFirst !== undefined && 
+     OperationSign !=='' &&
+     displayValue !== undefined ) {
+      console.log ('executre calcluation' )
+      //console.log(operate(OperationSign, Number(displayValueFirst),Number(displayValue)));
+      //console.log (typeof displayValueFirst )
+      displayValue = operate(OperationSign, Number(displayValueFirst),Number(displayValue));
+      //console.log(typeof displayValue + ', len:'+displayValue.toString().length )
+      if(displayValue.toString().length > 8) { 
+        if (displayValue < 99999999) {displayValue = displayValue.toPrecision(8)
+        } else {displayValue = displayValue.toPrecision(5)}
+      }
+      OperationSign = '';
+      displayValueFirst = undefined;
+      document.getElementById("display").textContent= displayValue;
+     }
+}
+
+function startFresh() {
+  //reset the values
+  displayValue = undefined;
+ displayValueFirst = undefined;
+ OperationSign = '';
+ document.getElementById("display").textContent = "0";
+ console.log('fresh start executed');
+
+}
+
+function executeSolo(Operation) {
+  //the inverse sign was pressed
+  if(displayValue !== undefined ) {
+     console.log ('executre: ' + Operation  + ' on:' + displayValue );
+     //console.log(operate(OperationSign, Number(displayValueFirst),Number(displayValue)));
+     //console.log (typeof displayValueFirst )
+     displayValue = operate(Operation,Number(displayValue));
+     console.log(Operation + ' res:' + displayValue );
+     //console.log(typeof displayValue + ', len:'+displayValue.toString().length )
+     if(displayValue.toString().length > 8) { 
+       if (displayValue < 99999999) {displayValue = displayValue.toPrecision(8)
+       } else {displayValue = displayValue.toPrecision(5)}
+     }
+     //OperationSign = '';
+     //displayValueFirst = undefined;
+     document.getElementById("display").textContent= displayValue;
+    }
+}
+
+function writePoint() {
+  //console.log('write point to display number');
+  let text1 = displayValue
+  //check there is no point in the number already
+  if (text1.search("\\.") === -1 ) {//no point found
+  console.log(typeof text1);
+  displayValue += '.'
+  document.getElementById("display").textContent= displayValue;
   }
 }
